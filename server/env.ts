6c5e27b005da1be6278c5c13c0084179c275e72f
Alan Luis Valenzuela Simpson <alan.v.simpson@hotmail.com>
2025-10-24 23:30:24 +0200

    fixed version dotenv

--- server/env.ts
+++ server/env.ts
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
-console.log("Loaded .env from:", path.resolve(process.cwd(), ".env"));
+console.log("Loaded .env from OK");
